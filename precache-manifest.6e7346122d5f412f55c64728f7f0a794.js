8a4e2458ec84ca5f2e7afe5740af71539fce06a4
jumagit <mukoovajuma183@gmail.com>
2019-07-23 16:11:44 +0300

    Updates

---
+++ precache-manifest.6e7346122d5f412f55c64728f7f0a794.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "945f6fbe934ea581125de17d8f383840",
+    "url": "/roboform/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/roboform/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a8ac63859b5b069ec592",
+    "url": "/roboform/static/css/main.cc8a01b0.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/roboform/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "a8ac63859b5b069ec592",
+    "url": "/roboform/static/js/main.4b94bd01.chunk.js"
+  },
+  {
+    "revision": "8716a12450eaa2e36bec",
+    "url": "/roboform/static/js/runtime~main.fa0bc5c1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboform/static/media/SEGA.12a3db8b.woff"
+  }
+]);
